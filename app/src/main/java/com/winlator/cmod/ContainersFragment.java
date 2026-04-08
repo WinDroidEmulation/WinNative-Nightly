a483660bef1d30019b49cc1ac0ad34232f68c639
ribbit384 <204555885+ribbit384@users.noreply.github.com>
2026-04-08 00:57:00 -0700

    chore: Remove unused resources and legacy assets (#142)
    chore: Remove unused resources, layouts, and legacy launcher icons
    
    - Delete GamepadConfiguratorDialog and its layout (replaced by Compose gamepad UI)
    - Remove all unused preference layouts, old XML preferences, and about dialog
    - Remove legacy launcher icons (gold, alt, monochrome, mono variants)
    - Remove unused drawables (ic_content_folder, ic_settings, logo, action bar icons)
    - Remove unused menus (containers_menu, extra_args_popup_menu)
    - Remove old shortcuts.xml and clean up leftover styles
    - Update ContainersFragment backup menu references

--- app/src/main/java/com/winlator/cmod/ContainersFragment.java
+++ app/src/main/java/com/winlator/cmod/ContainersFragment.java
@@ -5,9 +5,6 @@ import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -50,7 +47,6 @@ public class ContainersFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
         preloaderDialog = new PreloaderDialog(getActivity());
     }
 
@@ -106,22 +102,6 @@ public class ContainersFragment extends Fragment {
         ArrayList<Container> containers = manager.getContainers();
         recyclerView.setAdapter(new ContainersAdapter(containers));
         emptyTextView.setVisibility(containers.isEmpty() ? View.VISIBLE : View.GONE);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
-        // Menu removed to clean up header bar
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.containers_menu_add:
-                openAddContainer();
-                return true;
-            default:
-                return super.onOptionsItemSelected(menuItem);
-        }
     }
 
     private void showContainerBackupsDialog(Container container) {
